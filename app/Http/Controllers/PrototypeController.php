33c4f7dd45b2c7d1aaa985f945e8801a704c7e41
Gayden Lhendup <gaydenlhendup@MacBook-Pro.local>
2020-04-29 10:43:44 +0600

    first commit

---
+++ app/Http/Controllers/PrototypeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrototypeController extends Controller
+{
+    public function extensiondashboard(){
+
+       return view('prototype.extensiondashboard');
+    }
+}
