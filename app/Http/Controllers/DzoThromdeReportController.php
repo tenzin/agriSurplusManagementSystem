e144485ea40641c00b8983da8bc802f4ad6d1ff6
Gayden Lhendup <gaydenlhendup@MacBook-Pro.local>
2020-05-27 13:10:51 +0600

    fixed changes in report

--- app/Http/Controllers/DzoThromdeReportController.php
+++ app/Http/Controllers/DzoThromdeReportController.php
@@ -28,6 +28,9 @@ class DzoThromdeReportController extends Controller
     {
 
         $user = auth()->user();
+        $fromdate = $request->fromdate;
+        $todate = $request->todate;
+
         //surplus based on extension/farmer.
         if($request->rtype == "ex")
         {
@@ -72,24 +75,24 @@ class DzoThromdeReportController extends Controller
         {
             $title = "Surplus submitted by Commercial Aggregator";
 
-            $sql = "select tbl_product_types.type,tbl_products.product,tbl_cssurplus.quantity,tbl_units.unit,
-            tbl_ex_surplus.harvestDate,tbl_cssurplus.price,tbl_gewogs.gewog from tbl_cssurplus 
-            join tbl_transactions on tbl_transactions.id = tbl_cssurplus.trans_id
-            join tbl_product_types on tbl_cssurplus.productType_id = tbl_product_types.id
-            join tbl_products on tbl_cssurplus.product_id = tbl_products.id 
-            join tbl_units on tbl_cssurplus.unit_id = tbl_units.id
+            $sql = "select tbl_product_types.type,tbl_products.product,tbl_cssupply.quantity,tbl_units.unit,
+            tbl_cssupply.created_at as 'harvestDate',tbl_cssupply.price,'' as 'gewog' from tbl_cssupply 
+            join tbl_transactions on tbl_transactions.id = tbl_cssupply.trans_id
+            join tbl_product_types on tbl_cssupply.productType_id = tbl_product_types.id
+            join tbl_products on tbl_cssupply.product_id = tbl_products.id 
+            join tbl_units on tbl_cssupply.unit_id = tbl_units.id
             where tbl_transactions.status in ('S','E') and tbl_transactions.dzongkhag_id=".$user->dzongkhag_id;
             //date between.         
             if(!empty($fromdate) && !empty($todate))
             {
-            $sql = $sql. " and tbl_cssurplus.created_at between '".$fromdate."' and '".$todate."'";   
+            $sql = $sql. " and tbl_cssupply.created_at between '".$fromdate."' and '".$todate."'";   
             } 
             elseif(!empty($fromdate)){
-                $sql = $sql. " and tbl_cssurplus.created_at >= '".$fromdate."'";   
+                $sql = $sql. " and tbl_cssupply.created_at >= '".$fromdate."'";   
     
             }elseif( !empty($todate)) {
                 //only $todate is set.
-                $sql = $sql. " and tbl_cssurplus.created_at <= '".$todate."'"; 
+                $sql = $sql. " and tbl_cssupply.created_at <= '".$todate."'"; 
     
             }
             //    dd($sql);
@@ -108,7 +111,14 @@ class DzoThromdeReportController extends Controller
        // dd($sql);
         $surplus = DB::select($sql);
 
+        if($request->rtype == "ex")
+        {
         return view('DzoThromde.dzothromdereportdetails',compact('surplus','fromdate','todate','title'));
+        }
+        else
+        {
+            return view('DzoThromde.dzothromdecareportdetails',compact('surplus','fromdate','todate','title'));   
+        }
 
     }
 }
